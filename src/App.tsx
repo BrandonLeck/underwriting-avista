84f94e4d17add76cfe20f195d60cfbfbf7c51092
Brandon Lecklerh Solarte Melenje <brandonleck@mac.local>
2020-10-03 20:02:05 -0500

    feat (Navbar): Navbar component is used.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './App.css'
+import { Navbar } from 'core-avista/Navbar'
 
+const navItems = [
+  {
+    href: '#home',
+    description: 'Home',
+    current: true,
+  },
+  {
+    href: '#about',
+    description: 'About',
+    current: false,
+  },
+]
 function App() {
-  return <h1>Underwriting Avista</h1>
+  return <Navbar logo="Underwriting Avista" navItems={navItems} />
 }
 
 export default App
