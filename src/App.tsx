bd96dcb5b43e813c0c2c761b9a1b3bf38cf4d4d3
Brandon Lecklerh Solarte Melenje <brandonleck@mac.local>
2020-10-03 23:08:27 -0500

    refactor(Data): data is added to pass to components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import './App.css'
 import { Navbar } from 'core-avista/Navbar'
+import { Pricing } from './components/Pricing'
+import { navItems } from './data/navItems.json'
+import { offers } from './data/offers.json'
 
-const navItems = [
-  {
-    href: '#home',
-    description: 'Home',
-    current: true,
-  },
-  {
-    href: '#about',
-    description: 'About',
-    current: false,
-  },
-]
 function App() {
-  return <Navbar logo="Underwriting Avista" navItems={navItems} />
+  return (
+    <>
+      <Navbar logo="Underwriting Avista" navItems={navItems} />
+      <div className="container py-3">
+        <div className="pb-md-4 text-center">
+          <h1 className="display-4">Pricing</h1>
+          <p className="lead">
+            Quickly build an effective pricing table for your potential
+            customers with this Bootstrap example. It’s built with default
+            Bootstrap components and utilities with little customization.
+          </p>
+        </div>
+        <Pricing offers={offers} />
+      </div>
+    </>
+  )
 }
 
 export default App
